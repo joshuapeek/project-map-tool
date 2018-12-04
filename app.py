78af643e18fc45d46afd1368074d57db6b79ab9b
Joshua Peek <joshua.s.peek@gmail.com>
2018-12-03 20:47:25 -0600

    moved menu to content block to be more pythonic

--- app.py
+++ app.py
@@ -2,7 +2,12 @@ from flask import Flask, render_template, request, redirect
 from flask import url_for, flash, jsonify
 from sqlalchemy import create_engine, and_
 from sqlalchemy.orm import sessionmaker
-from database_setup import Org, Project, Role, Screen, Function, Functgroup, Action, Story, Section, Element, roleScreen, roleSection, storyScreen, user, userProject, userOrg, super, Base
+from database_setup import Org, Project, Role
+from database_setup import Screen, Function, Functgroup
+from database_setup import Action, Story, Section
+from database_setup import Element, roleScreen, roleSection
+from database_setup import storyScreen, user, userProject
+from database_setup import userOrg, super, Base
 from sqlalchemy.pool import StaticPool
 from flask import session as login_session
 import random
@@ -48,7 +53,7 @@ def mainPage():
 
 # query specified org object & all project objects associated with org
 # serves objects to org page template
-@app.route('/<int:org_id>/')
+@app.route('/o?<int:org_id>')
 def orgPage(org_id):
     allorgs = session.query(Org).all()
     allprojects = session.query(Project).all()
@@ -62,9 +67,9 @@ def orgPage(org_id):
 
 # query specified org & project objects
 # query projects objects associated with org
-# query function groups, screens, functions & role objects for specified project
+# query function groups, screens, functions & role objects for project
 # pass objects to project page template
-@app.route('/<int:org_id>/<int:project_id>/')
+@app.route('/o?<int:org_id>&p?<int:project_id>')
 def projectPage(org_id, project_id):
     allorgs = session.query(Org).all()
     allprojects = session.query(Project).all()
@@ -84,105 +89,8 @@ def projectPage(org_id, project_id):
 
 # CREATE Pages-------------------------
 
-# serves new org form for get request,
-# for post, searches for duplicate org title,
-#   if none found, adds form data to db
-@app.route('/new', methods=['GET', 'POST'])
-def newOrg():
-    if request.method == 'POST':
-        # check for org with same title
-        try:
-            check = session.query(Org).filter_by(title=request.form['orgtitle']).one()
-        except:
-            check = None
-        if check is not None:
-            flash("Org with this title already exists.")
-            return redirect(url_for('orgPage', org_id=check.id))
-        newOrg = Org(title=request.form['orgtitle'],
-                     description=request.form['orgdesc'])
-        session.add(newOrg)
-        session.commit()
-        flash("New org created!")
-        org = session.query(Org).filter_by(title=request.form['orgtitle']).one()
-        return redirect(url_for('orgPage',org_id=org.id))
-    else:
-        return render_template('create/org.html')
-
-
-# serves new org form for get request,
-# for post, searches for duplicate project titles within the org,
-#   if none found, adds form data to db
-@app.route('/newproject', methods=['GET', 'POST'])
-def newProject():
-    if request.method == 'POST':
-        org = session.query(Org).filter_by(title=request.form['org_title']).one()
-        unique= checkUniqueTitle(org.id, request.form['title'])
-        if unique == "confirmed":
-            newProject = Project(title=request.form['title'],
-                         description=request.form['description'],
-                         org_id=org.id)
-            session.add(newProject)
-            session.commit()
-            flash("New project created!")
-            # project = session.query(Project).filter_by(title=request.form['title']).one()
-            title=request.form['title']
-            project = session.query(Project).\
-                filter(Project.title==title, Project.org_id==org.id).one()
-            return redirect(url_for('projectPage',org_id=org.id, project_id=project.id))
-        else:
-            flash("Duplicate project detected")
-            return redirect(url_for('mainPage'))
-    else:
-        return render_template('create/org.html')
-
-def checkUniqueTitle(org_id, project_title):
-    projects = session.query(Project).filter_by(org_id=org_id).all()
-    unique = "1"
-    for i in projects:
-        if i.title == project_title:
-            unique="0"
-    if unique == "1":
-        unique = "confirmed"
-    return unique
-
-
-# serves new role form for get request, adds form data to db for post
-@app.route('/<int:project_id>/newrole', methods=['GET', 'POST'])
-def newRole(project_id):
-    project = session.query(Project).filter_by(id=project_id).one()
-    if request.method == 'POST':
-        newRole = Role(title=request.form['title'],
-                     description=request.form['description'],
-                     authRequired=request.form['authRequired'],
-                     project_id=project.id)
-        session.add(newRole)
-        session.commit()
-        flash("New role created!")
-        return redirect(url_for('projectPage',org_id=project.org.id, project_id=project.id))
-    else:
-        return render_template('create/role.html', project=project)
-
-
 # serves new screen form for get request, adds form data to db for post
-@app.route('/<int:project_id>/newscreen', methods=['GET', 'POST'])
-def newScreen(project_id):
-    project = session.query(Project).filter_by(id=project_id).one()
-    if request.method == 'POST':
-        newScreen = Screen(title=request.form['title'],
-                     description=request.form['description'],
-                     project_id=project.id,
-                     org_id=project.org.id)
-        session.add(newScreen)
-        session.commit()
-        flash("New Screen created!")
-        screen = session.query(Screen).filter_by(title=request.form['title']).one()
-        return redirect(url_for('projectPage',org_id=project.org.id, project_id=project_id))
-    else:
-        return render_template('create/screen.html', project=project)
-
-
-# serves new screen form for get request, adds form data to db for post
-@app.route('/newuser', methods=['GET', 'POST'])
+@app.route('/+u', methods=['GET', 'POST'])
 def newUser():
     if request.method == 'POST':
         org = session.query(Org).filter_by(title=request.form['orgtitle']).one()
@@ -203,15 +111,118 @@ def newUser():
         return redirect(url_for('mainPage'))
 
 
+# serves new org form for get request,
+# for post, searches for duplicate org title,
+#   if none found, adds form data to db
+@app.route('/+o', methods=['GET', 'POST'])
+def newOrg():
+    if request.method == 'POST':
+        # check for org with same title
+        try:
+            check = session.query(Org).filter_by(
+                title=request.form['orgtitle']).one()
+        except:
+            check = None
+        if check is not None:
+            flash("Org with this title already exists.")
+            return redirect(url_for('orgPage', org_id=check.id))
+        newOrg = Org(title=request.form['orgtitle'],
+                     description=request.form['orgdesc'])
+        session.add(newOrg)
+        session.commit()
+        flash("New org created!")
+        org = session.query(Org).filter_by(title=request.form['orgtitle']).one()
+        return redirect(url_for('orgPage',org_id=org.id))
+    else:
+        return render_template('create/org.html')
+
+
+# serves new org form for get request,
+# for post, searches for duplicate project titles within the org,
+#   if none found, adds form data to db
+@app.route('/+p', methods=['GET', 'POST'])
+def newProject():
+    if request.method == 'POST':
+        org = session.query(Org).filter_by(
+            title=request.form['org_title']).one()
+        unique= checkUniqueTitle(org.id, request.form['title'])
+        if unique == "confirmed":
+            newProject = Project(title=request.form['title'],
+                         description=request.form['description'],
+                         org_id=org.id)
+            session.add(newProject)
+            session.commit()
+            flash("New project created!")
+            session.refresh(newProject)
+            return redirect(url_for('projectPage',org_id=org.id,
+                                     project_id=newProject.id))
+        else:
+            flash("Duplicate project detected")
+            return redirect(url_for('mainPage'))
+    else:
+        return render_template('create/org.html')
+
+def checkUniqueTitle(org_id, project_title):
+    projects = session.query(Project).filter_by(org_id=org_id).all()
+    unique = "1"
+    for i in projects:
+        if i.title == project_title:
+            unique="0"
+    if unique == "1":
+        unique = "confirmed"
+    return unique
+
+
+# serves new role form for get request, adds form data to db for post
+@app.route('/p?<int:project_id>&+rle', methods=['GET', 'POST'])
+def newRole(project_id):
+    project = session.query(Project).filter_by(id=project_id).one()
+    if request.method == 'POST':
+        newRole = Role(title=request.form['title'],
+                     description=request.form['description'],
+                     authRequired=request.form['authRequired'],
+                     project_id=project.id)
+        session.add(newRole)
+        session.commit()
+        flash("New role created!")
+        return redirect(url_for('projectPage',org_id=project.org.id,
+                                 project_id=project.id))
+    else:
+        return render_template('create/role.html', project=project)
+
+
+# serves new screen form for get request, adds form data to db for post
+@app.route('/p?<int:project_id>&+scrn', methods=['GET', 'POST'])
+def newScreen(project_id):
+    project = session.query(Project).filter_by(id=project_id).one()
+    if request.method == 'POST':
+        newScreen = Screen(title=request.form['title'],
+                     description=request.form['description'],
+                     project_id=project.id,
+                     org_id=project.org.id)
+        session.add(newScreen)
+        session.commit()
+        flash("New Screen created!")
+        screen = session.query(Screen).filter_by(
+            title=request.form['title']).one()
+        return redirect(url_for('projectPage',org_id=project.org.id,
+                                 project_id=project_id))
+    else:
+        return render_template('create/screen.html', project=project)
+
+
+
 # serves new function form for get, adds form data to db for post
 #
 # NOTE: Adding Function, then Committing - this may cause an issue! Test!
 # Iterating over screens returned from multiselect, adding each to FunctScreen
-@app.route('/<int:project_id>/newfunction', methods=['GET', 'POST'])
+@app.route('/p?<int:project_id>&+fnctn', methods=['GET', 'POST'])
 def newFunction(project_id):
     targetproject = session.query(Project).filter_by(id=project_id).one()
-    projectscreens = session.query(Screen).filter_by(project_id=project_id).all()
-    roles = session.query(Role).filter_by(project_id=project_id).all()
+    projectscreens = session.query(Screen).filter_by(
+        project_id=project_id).all()
+    roles = session.query(Role).filter_by(
+        project_id=project_id).all()
     if request.method == 'POST':
         # receive form with title, description, authRequired, screens (list)
         newFunction = Function(title=request.form['title'],
@@ -229,7 +240,8 @@ def newFunction(project_id):
         #            function.project_id==targetproject.id).one()
         # screens = request.form.getlist('screens')
         flash("New Function created!")
-        return redirect(url_for('projectPage',org_id=targetproject.org.id, project_id=targetproject.id))
+        return redirect(url_for('projectPage',org_id=targetproject.org.id,
+                                 project_id=targetproject.id))
     else:
         return render_template('create/function.html', project=targetproject,
                                roles=roles, screens=projectscreens)
@@ -238,11 +250,9 @@ def newFunction(project_id):
 # UPDATE Pages-------------------------
 
 # query specified org, serve edit org page for get, update form data for post
-@app.route('/<int:org_id>/edit', methods=['GET', 'POST'])
+@app.route('/o?<int:org_id>&ed', methods=['GET', 'POST'])
 def editOrg(org_id):
     editOrg=session.query(Org).filter_by(id=org_id).one()
-    allorgs = session.query(Org).all()
-    allprojects = session.query(Project).all()
     if request.method == 'POST':
         if request.form['orgtitle']:
             editOrg.title = request.form['orgtitle']
@@ -257,7 +267,7 @@ def editOrg(org_id):
 
 # query specified org & project
 # serve edit project page for get, update form data for post
-@app.route('/<int:org_id>/<int:project_id>/edit', methods=['GET', 'POST'])
+@app.route('/o?<int:org_id>&p?<int:project_id>&ed', methods=['GET', 'POST'])
 def editProject(org_id, project_id):
     editOrg=session.query(Org).filter_by(id=org_id).one()
     editProject=session.query(Project).filter_by(id=project_id).one()
@@ -270,9 +280,35 @@ def editProject(org_id, project_id):
         session.add(editProject)
         session.commit()
         flash("Project Edited!")
-        return redirect(url_for('projectPage',org_id=editOrg.id, project_id=editProject.id))
+        return redirect(url_for('projectPage',org_id=editOrg.id,
+                                 project_id=editProject.id))
     else:
-        return render_template('editProject.html', org=editOrg, project=editProject)
+        return render_template('editProject.html', org=editOrg,
+                                project=editProject)
+
+
+# query specified role; find screens & sections from related project
+# serve edit role page for get, update db data for post
+@app.route('/rle?<int:role_id>&ed', methods=['GET', 'POST'])
+def editRole(role_id):
+    editRole=session.query(Role).filter_by(id=role_id).one()
+    screens=session.query(Screen).filter_by(
+        project_id=editRole.project.id).all()
+    sections=session.query(Section).filter_by(
+        project_id=editRole.project.id).all()
+    if request.method == 'POST':
+        if request.form['title']:
+            # THIS NEEDS TO BE DONE
+            editRole.title = request.form['title']
+            editRole.description = request.form['desc']
+        session.add(editRole)
+        session.commit()
+        flash("Role Edited!")
+        return redirect(url_for('projectPage',org_id=editRole.project.org.id,
+                                 project_id=editRole.project.id))
+    else:
+        return render_template('editProject.html', editRole=editRole,
+            section=sections, screens=screens)
 
 
 # DELETE Pages-------------------------
@@ -280,7 +316,7 @@ def editProject(org_id, project_id):
 # query specified org, serve delete form for get
 # on post, cycle-delete all project elements per org,
 #   then projects per org, then finally - org
-@app.route('/<int:org_id>/del', methods=['GET', 'POST'])
+@app.route('/o?<int:org_id>&d', methods=['GET', 'POST'])
 def delOrg(org_id):
     delOrg = session.query(Org).filter_by(id=org_id).one()
     delProjects = session.query(Project).filter_by(org_id=org_id).all()
@@ -300,7 +336,7 @@ def delOrg(org_id):
 
 # query specified project, org; serve delete form for get, delete db for post
 # iterate to remove all project elements before deleting project
-@app.route('/<int:org_id>/<int:project_id>/del', methods=['GET', 'POST'])
+@app.route('/o?<int:org_id>&o?<int:project_id>&d', methods=['GET', 'POST'])
 def delProject(org_id, project_id):
     delOrg = session.query(Org).filter_by(id=org_id).one()
     delProject = session.query(Project).filter_by(id=project_id).one()
@@ -329,24 +365,24 @@ def remProjectElements(project_id):
 
 # query specified role, observe org & project id's in role object
 # serve delete form for get; on post, delete function
-@app.route('/roledel/<int:role_id>', methods=['GET', 'POST'])
+@app.route('/rle?<int:role_id>&d', methods=['GET', 'POST'])
 def delRole(role_id):
     delRole = session.query(Role).filter_by(id=role_id).one()
-    org_id=delRole.org_id
-    project_id=delRole.project_id
+    project = session.query(Project).filter_by(id=delRole.project.id).one()
+    org_id=project.org.id
     if request.method == 'POST':
         session.delete(delRole)
         session.commit()
         flash("Role Removed")
         return redirect(url_for('projectPage',org_id=org_id,
-                                 project_id=project_id))
+                                 project_id=project.id))
     else:
-        return render_template('delete/role.html', i=delRole)
+        return render_template('delete/role.html', i=delRole, project=project)
 
 
 # query specified screen, observe org & project id's in role object
 # serve delete form for get; on post, delete function
-@app.route('/_screendel/<int:screen_id>', methods=['GET', 'POST'])
+@app.route('/scrn?<int:screen_id>&d', methods=['GET', 'POST'])
 def delScreen(screen_id):
     delScreen = session.query(Screen).filter_by(id=screen_id).one()
     org_id=delScreen.org.id
@@ -365,7 +401,7 @@ def delScreen(screen_id):
 
 # query specified function, observe org & project id's in role object
 # serve delete form for get; on post, delete function
-@app.route('/functiondel/<int:function_id>', methods=['GET', 'POST'])
+@app.route('/fnctn?<int:function_id>&d', methods=['GET', 'POST'])
 def delFunction(function_id):
     delFunction = session.query(Function).filter_by(id=function_id).one()
     org_id=delFunction.org_id
@@ -383,7 +419,7 @@ def delFunction(function_id):
 # receive user_id, query userOrg (and in the future, userProject) rows matching,
 # delete userOrg rows, then user record
 # serve delete form for get; on post, delete user
-@app.route('/userdel/<int:user_id>', methods=['GET', 'POST'])
+@app.route('/u?<int:user_id>&d', methods=['GET', 'POST'])
 def delUser(user_id):
     delUser = session.query(user).filter_by(id=user_id).one()
     delUserOrg = session.query(userOrg).filter_by(user_id=user_id).all()
@@ -402,7 +438,7 @@ def delUser(user_id):
 
 # JSON Project page:
 # displays project info in db, serialized
-@app.route('/<int:org_id>/<int:project_id>/JSON')
+@app.route('/o?<int:org_id>&p?<int:project_id>/JSON')
 def projectJSON(org_id, project_id):
     org = session.query(Org).filter_by(id=org_id).one()
     project = session.query(Project).filter_by(
@@ -412,7 +448,7 @@ def projectJSON(org_id, project_id):
 
 # JSON Org page:
 # displays org info in db, serialized
-@app.route('/<int:org_id>/JSON')
+@app.route('/o?<int:org_id>/JSON')
 def orgJSON(org_id):
     org = session.query(Org).filter_by(id=org_id).one()
     projects = session.query(Project).filter_by(org_id=org_id).all()
@@ -422,7 +458,7 @@ def orgJSON(org_id):
 
 # JSON Org Projects page:
 # displays info for all of an orgs projects in db, serialized
-@app.route('/<int:org_id>/projects/JSON')
+@app.route('/o?<int:org_id>/projects/JSON')
 def orgProjectsJSON(org_id):
     org = session.query(Org).filter_by(id=org_id).one()
     projects = session.query(Project).filter_by(org_id=org_id).all()
